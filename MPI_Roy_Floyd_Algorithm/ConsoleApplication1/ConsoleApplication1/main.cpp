4de65e00bf1ce81fa364f6a6b2b6aabe7634eea7
Gabi\Gabriel <gdincagabriel@gmail.com>
2018-05-03 20:24:12 +0300

    Added MPI implementation for Roy Floyd algorithm (failing because of stack error)

--- MPI_Roy_Floyd_Algorithm/ConsoleApplication1/ConsoleApplication1/main.cpp
+++ MPI_Roy_Floyd_Algorithm/ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,136 +1,64 @@
-/*#include <mpi.h>
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-	// Initialize the MPI environment
-	MPI_Init(NULL, NULL);
-
-	// Get the number of processes
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	// Get the rank of the process
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-	// Get the name of the processor
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-
-
-	// Finalize the MPI environment.
-	MPI_Finalize();
-}*/
-
+#include <mpi.h>
 #include<stdio.h>
 #include<conio.h>
-#define pinf 10000 
-int a[20][20], n, m;
 
-void init_cost_mat()
-{
-	FILE *f;
-	errno_t err;
-	int i, j, x, y, c;
-	err = fopen_s(&f, "roy_in.txt", "rt");
-	if (err)
-		printf("File failed to open");
-
-	printf("\n");
-	fscanf_s(f, "%d", &n);
-	fscanf_s(f, "%d\n", &m);
-	//initializare matrice
-	for (i = 1;i <= n;i++)
-		for (j = 1;j <= n;j++)
-			if (i == j)
-				a[i][j] = 0;
-			else
-				a[i][j] = pinf;
-
-	for (i = 1;i <= m;i++)
-	{
-		fscanf_s(f, "%d %d %d\n", &x, &y, &c);
-		a[x][y] = a[y][x] = c;
-	}
-	fclose(f);
-}
-
-void print_mat()
-{
-	for (int i = 1;i <= n;i++)
-	{
-		for (int j = 1;j <= n;j++)
-			if (a[i][j] == pinf)
-				printf(" - ");
-			else
-				printf("%4d ", a[i][j]);
-		printf("\n");
-	}
-}
-
-void RoyFloyd() // roy floyd
-{
-	for (int k = 1;k <= n;k++)
-		for (int i = 1;i <= n;i++)
-			for (int j = 1;j <= n;j++)
-				if (a[i][j]>a[i][k] + a[k][j])
-				{
-					a[i][j] = a[i][k] + a[k][j];
-				}
-}
-
-void descompun_drum(int i, int j)
-{
-	int ok = 0, k = 1;
-
-	while (k <= n && !ok)
-	{
-		if (i != k&&j != k)
-			if (a[i][j] == a[i][k] + a[k][j])
-			{
-				descompun_drum(i, k);
-				descompun_drum(k, j);
-
-				ok = 1;
-			}
-			k++;
-	}
-	if (!ok)
-	{
-		printf(" %d ", j);
-	}
-
-}
-
-void scriu_drum(int nod_initial, int nod_final)
-{
-	if (a[nod_initial][nod_final]<pinf)
-	{
-		printf("Path from %d to %d has the cost: %d",nod_initial,nod_final,a[nod_initial][nod_final]);
-		printf("\n Best path is: %d ", nod_initial);
-		descompun_drum(nod_initial, nod_final); 
-	}
-	else
-		printf("There's no path between %d and %d ", nod_initial, nod_final);
-
-}
-
+#define inf 10000
+#define SIZE 5
 
 void main()
 {
-	int x, y;
-	init_cost_mat();
-	printf("\nThe cost matrix: \n");
-	print_mat();
-	RoyFloyd();
-	printf("\nOptim cost matrix\n");
-	print_mat();
-	printf("\nFind the best path between x and y \n");
-	printf("x=");
-	scanf_s("%d", &x);
-	printf("y=");
-	scanf_s("%d", &y);
-	scriu_drum(x, y);
-	_getch();
+	int cost_matrix[SIZE][SIZE] = {
+		{0, 2, inf, 2, inf},
+		{2, 0, 3, inf, inf}, 
+		{inf, 3, 0, 1, 8},
+		{10, inf, 1, 0, inf},
+		{inf, inf, 8, inf, 0} 
+	};
+
+	int optimal_cost_matrix[SIZE][SIZE];
+
+	int numprocs, procid, len, partner, message;
+	char hostname[MPI_MAX_PROCESSOR_NAME];
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &procid);
+	MPI_Get_processor_name(hostname, &len);
+	MPI_Status status;
+
+	for (int i = 0;i < SIZE;i++)
+	{
+		for (int j = 1;j < SIZE;j++)
+		{
+			if (cost_matrix[i][j] > cost_matrix[i][procid] + cost_matrix[procid][j])
+			{
+				cost_matrix[i][j] = cost_matrix[i][procid] + cost_matrix[procid][j];
+			}
+		}
+	}
+
+	MPI_Gather(cost_matrix, SIZE*SIZE, MPI_INT, optimal_cost_matrix, SIZE*SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (procid == 0)
+	{
+		for (int i = 0;i < SIZE;i++)
+		{
+			for (int j = 1;j < SIZE;j++)
+			{
+				cost_matrix[i][j] = optimal_cost_matrix[i][j];
+			}
+		}
+
+		printf("Optimal cost matrix is: ");
+
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				printf("%d ", cost_matrix[i][j]);
+			}
+			printf("\n");
+		}
+
+	}
+
+	
+	MPI_Finalize();
 }
